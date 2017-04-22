897a0a0009a8c84975cae8148ee8a81e11e0072a
Robert Homewood <roberthomewood@Roberts-MacBook-Pro.local>
2017-04-22 17:00:19 +0100

    added very basic gui support

--- sketch.js
+++ sketch.js
@@ -1,8 +1,13 @@
+var dummyVar;
+
 function setup() {
 createCanvas(windowWidth, windowHeight);
+dummyVar = 200;
+var gui = createGui('Label');
+gui.addGlobals('dummyVar');
 }
 
 function draw() {
 	background(0);
-  ellipse(20, 50, 80, 80);
+  ellipse(width / 2, height / 2, dummyVar, 80);
 }
