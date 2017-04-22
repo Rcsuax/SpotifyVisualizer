812c8ae6a2004e3d436c8959717b3f3c3bee8d78
rcsuax <rcsuax@gmail.com>
2017-04-22 23:20:33 +0100

    working api endpoint

--- src/controllers/spotifyController.js
+++ src/controllers/spotifyController.js
@@ -1,45 +1,20 @@
 import express from 'express';
 import path from 'path';
-import https from 'https';
-import request from 'request';
-import parser from 'parse-json';
+import axios from 'axios';
 
 let router = express.Router();
 
-router.get('/', (req,res) =>
-    res.send(getData())
+router.get('/', (req,res) => {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then(function (response) {
+        res.send(response.data);
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+}
+
 );
 
 
 module.exports = router
-
-function getData(){
-    let options = {
-        host : 'api.spotify.com',
-        path : '/v1/audio-analysis/3JIxjvbbDrA9ztYlNcp3yL',
-        port : 443,
-        'Content-Length': Buffer.byteLength(100000),
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-            Authorization: 'Bearer BQAT-f5Rgt8srhNCjWN_nABr2MOaUhiLgO7o8mR362oW13DE3VXCcprV71dMC4BJx-ADv_tBxdoWs5pRbiYSSwWiGZvOgqwNJu1pLn3UH9jIIvZJwCIDQX2RXM5ib7rZ__qdag'
-        }
-    };
-
-    let request = https.get(options, function(response) {
-        let result = '';
-        console.log(response.statusCode);
-
-        response.on('data', function(chunk) {
-            result += chunk;
-        });
-
-        response.on('end', function() {
-            console.log(result);
-        });
-
-        request.on('error', function(e) {
-          console.error(e);
-        });
-    });
-}
