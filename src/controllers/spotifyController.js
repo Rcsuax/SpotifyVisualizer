499c3b2a91bfd4229f5077fa86f79ab651e82cfc
rcsuax <rcsuax@gmail.com>
2017-04-23 00:56:36 +0100

    process json data, not sure if it works

--- src/controllers/spotifyController.js
+++ src/controllers/spotifyController.js
@@ -6,7 +6,15 @@ let router = express.Router();
 
 router.get('/', (req,res) => {
     const headers = {
-        headers: {'Authorization': 'Bearer BQA4vs-rqhbUq-T2MCg2ULJL0rBZdap2mCZnVOT55pPCwLTiDGnSYOj3P6M2nn0DLQlbBKmVBuuGOkRsLQaohDiFsYlNnoWKkBnupjSZMSXis2bT9-4dUwmggODNncZQ3jno0g'}
+        headers: {'Authorization': 'Bearer BQA4vs-rqhbUq-T2MCg2ULJL0rBZdap2mCZnVOT55pPCwLTiDGnSYOj3P6M2nn0DLQlbBKmVBuuGOkRsLQaohDiFsYlNnoWKkBnupjSZMSXis2bT9-4dUwmggODNncZQ3jno0g'},
+        transformResponse: [function (data) {
+            let json = JSON.parse(data)
+            let track = { 'duration': json.track.duration , 'bars' : [] } //{'start': json.segments.start, 'amplitude':json.segments.loudness_max}
+            json.segements.forEach(function() {
+                track.bars.unshift({'start': json.segments.start, 'amplitude':json.segments.loudness_max})
+            })
+            return track;
+        }],
     }
     axios.get('https://api.spotify.com/v1/audio-analysis/3JIxjvbbDrA9ztYlNcp3yL',headers)
     .then(function (response) {
