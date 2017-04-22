988606a68db721904fbd5ac9507b84c3ce735abe
Amy Cartwright <amycartwright@Amys-MBP.eduroam.student>
2017-04-22 18:26:40 +0100

    first wave test

--- src/views/assets/sketch.js
+++ src/views/assets/sketch.js
@@ -1,13 +1,48 @@
-var dummyVar;
+var radius;
+var totalRays;
+var noiseMax;
+var angleStep;
+var noiseSeeds;
+var x;
+var y;
+var posx;
+var posy;
+var curRadius
 
 function setup() {
-createCanvas(windowWidth, windowHeight);
-dummyVar = 200;
-var gui = createGui('Label');
-gui.addGlobals('dummyVar');
+    createCanvas(windowWidth, windowHeight);
+    radius = 200;
+    totalRays = 50;
+    noiseMax = 300;
+    angleStep = 365/totalRays;
+    noiseSeeds = random(10000);
+
 }
 
 function draw() {
-	background(255);
-  ellipse(width / 2, height / 2, dummyVar, 80);
+	background(0);
+	noFill();
+    stroke(random(255), random(255), random(255));
+	smokeyCircle(width/2, height/2);
+}
+
+function smokeyCircle(x, y){
+	
+    push();
+    translate(x,y);
+	beginShape();
+    
+   for (var i = 0; i < totalRays; i++) {
+        curRadius =  radius + noise(noiseSeeds) * noiseMax; //Creates movement
+        posx = sin((angleStep*i)) * curRadius; //Distributes x points
+        posy = cos((angleStep*i)) * curRadius; //Distributes y points
+
+        vertex(posx,posy);
+        noiseSeeds += 0.01; //Increment all by 0.01
+
+        //console.log();
+    }
+    endShape(CLOSE);
+    pop();
+    
 }
