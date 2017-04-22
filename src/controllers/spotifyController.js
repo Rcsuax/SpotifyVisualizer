4047c68d9c265814bef9faad58e78b6e84abdd07
rcsuax <rcsuax@gmail.com>
2017-04-22 23:53:53 +0100

    api returns track analysis

--- src/controllers/spotifyController.js
+++ src/controllers/spotifyController.js
@@ -5,7 +5,10 @@ import axios from 'axios';
 let router = express.Router();
 
 router.get('/', (req,res) => {
-    axios.get('https://jsonplaceholder.typicode.com/posts')
+    const headers = {
+        headers: {'Authorization': 'Bearer BQA4vs-rqhbUq-T2MCg2ULJL0rBZdap2mCZnVOT55pPCwLTiDGnSYOj3P6M2nn0DLQlbBKmVBuuGOkRsLQaohDiFsYlNnoWKkBnupjSZMSXis2bT9-4dUwmggODNncZQ3jno0g'}
+    }
+    axios.get('https://api.spotify.com/v1/audio-analysis/3JIxjvbbDrA9ztYlNcp3yL',headers)
     .then(function (response) {
         res.send(response.data);
     })
